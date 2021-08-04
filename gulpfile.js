c284e8befa05b9117167855afc7decde3e869716
Simon Gagnon <simgagnon@Simons-MacBook-Air.local>
2021-08-03 20:01:48 -0400

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,46 @@
+// Initialize modules
+const { src, dest, watch, series} = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+const babel = require('gulp-babel');
+const browsersync = require('browser-sync').create();
+
+// Sass task
+function scssTask() {
+    return src('./scss/*.scss', { sourcemaps: true})
+    .pipe(sass())
+    .pipe(postcss([autoprefixer(), cssnano()]))
+    .pipe(dest('./css', { sourcemaps: '.'}));
+}
+
+// Browsersync
+function browserSyncServer(cb) {
+    browsersync.init({
+        server: {
+            baseDir: '.',
+        },
+        notify: {
+            styles: {
+                top: 'auto',
+                bottom: '0',
+            },
+        },
+    });
+    cb();
+}
+function browsserSyncReload(cb) {
+    browsersync.reload();
+    cb();
+}
+
+// Watch Task
+function watchTask() {
+    watch('*.html', browsserSyncReload);
+    watch(['./scss/*.scss'],
+    series(scssTask, browsserSyncReload));
+}
+
+// Default Gulp Task
+exports.default = series(scssTask, browserSyncServer, watchTask);
